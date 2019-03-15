86e7b17cb11b61678b7c49fa763cedc2fb123052
Alexandru <alexandru.ionut.rosu@gmail.com>
2019-03-15 03:00:52 +0200

    Added files

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,39 @@
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
 
 module.exports = {
    entry: {
-       app: './src/index.js'
+        app: ['./src/js/index.js', './src/css/style.scss']
    },
    output: {
-       path: path.resolve(__dirname, 'dist'),
-       filename: 'main.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
    },
+   plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Custom template',
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: [
+                    autoprefixer()
+                ]
+            }
+        })
+   ],
    module: {
        rules: [{
            test: /\.js?$/,
@@ -16,6 +42,17 @@ module.exports = {
            query: {
                presets: ['env']
            }
-       }]
+        },
+        {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                "postcss-loader",
+                "sass-loader"
+            ]
+        }
+    ]
    }
 }
